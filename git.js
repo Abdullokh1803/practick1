3d8262b15db956768bc08d227fad10f080e9171b
abdullokh1803 <abdullokhm2005@gmail.com>
2023-12-05 11:15:36 +0600

    update

--- git.js
+++ git.js
@@ -9,9 +9,10 @@
 4.  git init                // интциолизация в git
 5.  git add .               // публикация всех папок в git
 6.  git commit -m "init"    
+    и 3 команды с github
 
             Git Updates
-            
+
 7.  git add .
 8.  git commit -m 'update'
 9.  git push origin main 
