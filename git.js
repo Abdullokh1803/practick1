0752f44676103651bfe8c129ef69fad9f6382014
abdullokh1803 <abdullokhm2005@gmail.com>
2023-12-05 10:39:08 +0600

    first commit

---
+++ git.js
@@ -0,0 +1,12 @@
+/*
+
+1.  cd ..                   // назад
+2.  ls                      // Информация
+3.  cd fileName             // вход в папку
+
+4.  git comands             // 
+5.  git init                // интциолизация в git
+6.  git add .               // публикация всех папок в git
+7.  git commit -m "init"    
+
+*/
