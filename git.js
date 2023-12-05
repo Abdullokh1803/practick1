ac415822943fb4fdf1d7e5072981d65a3412dd71
abdullokh1803 <abdullokhm2005@gmail.com>
2023-12-05 10:47:13 +0600

    update

--- git.js
+++ git.js
@@ -4,9 +4,16 @@
 2.  ls                      // Информация
 3.  cd fileName             // вход в папку
 
-4.  git comands             // 
-5.  git init                // интциолизация в git
-6.  git add .               // публикация всех папок в git
-7.  git commit -m "init"    
+            Git Comands
+
+4.  git init                // интциолизация в git
+5.  git add .               // публикация всех папок в git
+6.  git commit -m "init"    
+
+            Git Updates
+            
+7.  git add .
+8.  git commit -m 'update'
+9.  git push origin main 
 
 */
